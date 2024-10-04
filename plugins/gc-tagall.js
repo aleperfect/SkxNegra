99aa4617651a9761624d9ca623f2ccc2e117b0b2
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-10-04 18:32:57 -0500

    dfgdfg

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,10 +6,10 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
   let pesan = args.join` `
   let oi = `*𝐌𝐞𝐧𝐬𝐚𝐣𝐞:* ${pesan}`
   let teks =`🫶🏻 Vende o compra cuentas seguras ✅
- Aqui 👉🏻https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY\n\n🌩️ ${oi}\n\n⚽ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚:*\n`
+ Aqui 👉🏻https://www.instagram.com/gswxteam/\n\n🌩️ ${oi}\n\n⚽ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚:*\n`
   for (let mem of participants) {
-  teks += `▌🎠 @${mem.id.split('@')[0]}\n`}
-  teks += `◥ 𝐁𝐥𝐮𝐞 𝐋𝐨𝐜𝐤 ◤`
+  teks += `⭐🎠 @${mem.id.split('@')[0]}\n`}
+  teks += `GSW `
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
   handler.help = ['tagall <mesaje>','invocar <mesaje>']
--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -33,9 +33,9 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `⫷⌧⌧⌧⌧ ⚡ ⌧⌧⌧⌧⪢
-🚀 Creador : Readz Aleizn
-🚀 Wa.me/51992621601
-🚀 Ventas : https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY
+🚀 Creador : GSW
+🚀 Wa.me/5493584386695
+🚀 Ventas : https://www.instagram.com/gswxteam/
 ⫷⌧⌧⌧⌧ 💘 ⌧⌧⌧⌧⪢
 
 
